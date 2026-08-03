b212f7fb45d36bbe9e9c4026089280a14fab257c
ClaudioFritzen <sdfritzen96@gmail.com>
2026-08-03 21:09:05 +0100

    modificamos para utilizar o rabbitmq

--- app/celery_app.py
+++ app/celery_app.py
@@ -3,8 +3,9 @@ from celery import Celery
 
 app = Celery(
     'hello_celery',
-    broker='redis://redis_queue:6379/0',
-    backend='redis://redis_queue:6379/1',
+    broker='amqp://guest:guest@rabbitmq:5672//',
+    backend='rpc://',
+    #backend='redis://redis_queue:6379/1',
 )
 
 app.conf.update(
